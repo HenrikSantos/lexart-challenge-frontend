53b5a0e3f1a36b3416b2aadcdd5d5573050ac4d4
Henrik Santos <henrik.ruan4@gmail.com>
2023-08-26 16:06:24 -0300

    🎨 custom greetings

--- src/utils/handleStartConversation.tsx
+++ src/utils/handleStartConversation.tsx
@@ -6,13 +6,27 @@ interface IHandleStartConversation {
   setMessages: React.Dispatch<React.SetStateAction<IMessage[]>>,
 }
 
+function getGreeting(message: string) {
+  const lowerCaseMessage = message.toLowerCase();
+
+  if (lowerCaseMessage.includes("goodbye")) {
+    return "Welcome! Login with your username and password, ex: myname mypassword, after that, you can end the chat by writing \"goodbye\", and then the chat will be exported to the chatdata page";
+  } else if (lowerCaseMessage.includes("good")) {
+    return "Good day!, Login with your username and password, ex: myname mypassword.";
+  } else if (lowerCaseMessage.includes("i want")) {
+    return "Welcome! Log in with your username and password, ex: myname mypassword, and I'll see what I can do for you!";
+  }
+
+  return "Welcome! Login with your username and password, ex: myname mypassword";
+}
+
 export default function handleStartConversation({ message, setMessages }: IHandleStartConversation) {
   const keywords = ["hello", "goodbye", "good", "i want"];
   const formatedMessage = message.toLowerCase();
 
   if (keywords.some((keyword) => formatedMessage.includes(keyword))) {
     const newMessage: IAddNewMessage = {
-      message: "Welcome! Login with your username and password, ex: myname mypassword",
+      message: getGreeting(message),
       side: "left",
       id: "requestLogin",
       setMessages
